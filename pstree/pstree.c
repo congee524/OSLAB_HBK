5fb40e8ec93565eb4d6a8a7d6a4d1b0c49ef097f
tracer-nju <tracer@nju.edu.cn>
2019-02-26 14:50:00 +0800

    Static hostname: Inspiron          Icon name: computer-laptop            Chassis: laptop         Machine ID: 537ec2aa89e64954b7d607f1de70fb4e            Boot ID: 18e7d594f888409296a08f287e7aea9e   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.15.0-46-generic       Architecture: x86-64  14:50:01 up 1 day,  3:28,  2 users,  load average: 0.68, 1.16, 1.27

--- pstree/pstree.c
+++ pstree/pstree.c
@@ -2,12 +2,33 @@
 #include <assert.h>
 
 int main(int argc, char *argv[]) {
-  printf("Hello, World!\n");
-  int i;
-  for (i = 0; i < argc; i++) {
-    assert(argv[i]); // always true
-    printf("argv[%d] = %s\n", i, argv[i]);
-  }
-  assert(!argv[argc]); // always true
-  return 0;
+    printf("Hello, World!\n");
+    int i;
+    for (i = 0; i < argc; i++) {
+        assert(argv[i]); // always true
+        printf("argv[%d] = %s\n", i, argv[i]);
+    }
+    assert(!argv[argc]); // always true
+    int ch;
+    while((ch = getopt(argc, argv, "p:n:V:"))) {
+        printf("optind: %d\n", optind);
+        switch(ch) {
+            case 'p':
+                printf("Have opotion: -p\n");
+                aflag = 1;
+                break;
+            case 'n':
+                printf("Have option: -n\n");
+                aflag = 1;
+                break;
+            case 'V':
+                printf("Have option: -V\b");
+                aflag = 1;
+                break;
+            case '?':
+                printf("Unknown option: %c\n", (char)optopt);
+                break;
+        }
+    }
+    return 0;
 }
