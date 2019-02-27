f74b6e4ceb6e75370978921a99a8f1e464cc65ea
tracer-nju <tracer@nju.edu.cn>
2019-02-27 09:46:03 +0800

    Static hostname: Inspiron          Icon name: computer-laptop            Chassis: laptop         Machine ID: 537ec2aa89e64954b7d607f1de70fb4e            Boot ID: 18e7d594f888409296a08f287e7aea9e   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.15.0-46-generic       Architecture: x86-64  09:46:03 up 1 day, 22:24,  4 users,  load average: 1.12, 1.23, 1.25

--- pstree/pstree.c
+++ pstree/pstree.c
@@ -130,7 +130,7 @@ int main(int argc, char *argv[]) {
     // printf("%d%d%d\n", p_flag, n_flag, v_flag);
 
     if (v_flag == 1) {
-        printf("pstree(minilab) 0.1\n");
+        printf("pstree(minilab) 0.1 version!\n");
         printf("OSminilab, the supported arguments are:\n");
         printf("-p(show-pids), -n(numeric-sort), -V(version).");
         return 0;
