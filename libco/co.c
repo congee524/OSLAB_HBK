9828f55f34af229052853ff75e6d34cc3509f6a4
tracer-nju <tracer@nju.edu.cn>
2019-03-21 21:57:35 +0800

    Static hostname: Inspiron          Icon name: computer-laptop            Chassis: laptop         Machine ID: 537ec2aa89e64954b7d607f1de70fb4e            Boot ID: b0bbf33baaf441e294b404a58189d5d3   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.15.0-47-generic       Architecture: x86-64  21:57:35 up 2 days,  1:44,  5 users,  load average: 0.95, 1.55, 1.36

--- libco/co.c
+++ libco/co.c
@@ -56,7 +56,7 @@ struct co* co_start(const char *name, func_t func, void *arg) {
 
     current = pre;
 
-    if (!setjmp(coroutine[pre].env)) {
+    if (setjmp(coroutine[pre].env)) {
         return (struct co*)(coroutine + pre);
     }
 
@@ -68,7 +68,7 @@ struct co* co_start(const char *name, func_t func, void *arg) {
 void co_yield() {
     int go;
     for (go = 0; go < MAXTHREAD; go++) {
-        if (coroutine[go].used == true && go != current) {
+        if (coroutine[go].used == true) {
             break;
         }
     }
