60ca200bbb0ca464c6fb385f13a284c93db8ee43
171240513 <171240513@smail.nju.edu.cn>
2019-03-21 20:43:45 +0800

    design the first edition

--- libco/co.c
+++ libco/co.c
@@ -1,23 +1,61 @@
 #include <stdio.h>
+#include <assert.h>
+#include <string.h>
+#include <setjmp.h>
+#include <stdlib.h>
 #include "co.h"
 
-struct co {
-    char *name;
-    func_t func;
+#define false 0
+#define true 1
 
+struct co {
+    char name[128];
+    func_t func;
+    jmp_buf env; // to save the state of present function
+    int used; // record the routine whether being used
+    void *coarg;
 };
 
+#define MAXTHREAD 10
+#define STACKDIR - // set - for downwards
+#define STACKSIZE (1 << 12)
+static struct co coroutine[MAXTHREAD];
+static int co_cnt; // to record the num of coroutine
+static void *tos; // top of stack
+
+
 void co_init() {
+    co_cnt = 0;
 }
 
 struct co* co_start(const char *name, func_t func, void *arg) {
-  func(arg); // Test #2 hangs
-  return NULL;
+    if (co_cnt >= MAXTHREAD) {
+        printf("NO ENOUGH THREADS!\n");
+        assert(0);
+    }
+
+    strcpy(coroutine[co_cnt].name, name);
+    // coroutine[co_cnt].name = name;
+    coroutine[co_cnt].func = func;
+    coroutine[co_cnt].used = true;
+
+    if (tos == NULL) {
+        tos = (void *)&arg;
+    }
+    tos += STACKDIR STACKSIZE;
+    char arg_n[STACKDIR(tos - (void *)&arg)];
+    coroutine[co_cnt].coarg = arg_n;
+
+    co_cnt++;
+    func(arg); // Test #2 hangs
+    return (struct co*)(coroutine + (co_cnt - 1));
 }
 
 void co_yield() {
 }
 
 void co_wait(struct co *thd) {
+    memset()
+    co_cnt--;
 }
 
--- libco/co.h
+++ libco/co.h
@@ -1,8 +1,15 @@
 #ifndef __CO_H__
 #define __CO_H__
 
+// #define MAXTHREAD 10
+
 typedef void (*func_t)(void *arg);
 struct co;
+
+/*
+co coroutine[MAXTHREAD];
+int co_cnt
+*/
 
 void co_init();
 struct co* co_start(const char *name, func_t func, void *arg);
