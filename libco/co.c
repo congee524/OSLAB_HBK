4c00903562a279aa06e8c4daac06146c9086f95d
tracer-nju <tracer@nju.edu.cn>
2019-03-22 09:21:50 +0800

    Static hostname: Inspiron          Icon name: computer-laptop            Chassis: laptop         Machine ID: 537ec2aa89e64954b7d607f1de70fb4e            Boot ID: b0bbf33baaf441e294b404a58189d5d3   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.15.0-47-generic       Architecture: x86-64  09:21:50 up 2 days, 13:09,  8 users,  load average: 0.85, 0.97, 1.19

--- libco/co.c
+++ libco/co.c
@@ -3,24 +3,28 @@
 #include <string.h>
 #include <setjmp.h>
 #include <stdlib.h>
+#include <stdint.h>
 #include "co.h"
 
-#define false 0
-#define true 1
+#define COROUTINE_DEAD 0
+#define COROUTINE_READY 1
+#define COROUTINE_RUNNING 2
+#define COROUTINE_SUSPEND 3
 
 struct co {
     char name[128];
     func_t func;
-    jmp_buf env; // to save the state of present function
-    int used; // record the routine whether being used
+    jmp_buf buf; // to save the state of present function
+    int state; // record the state of routine
     void *coarg;
+    uint8_t stack[4096];
 };
 
-#define MAXTHREAD 10
+#define MAX_CO 10
 #define STACKDIR - // set - for downwards
 #define STACKSIZE (1 << 12)
-static struct co coroutine[MAXTHREAD];
-static int current;
+static struct co coroutine[MAX_CO];
+static struct co *current;
 // static int co_cnt; // to record the num of coroutine
 static void *tos; // top of stack
 
@@ -32,12 +36,12 @@ void co_init() {
 
 struct co* co_start(const char *name, func_t func, void *arg) {
     int pre;
-    for (pre = 0; pre < MAXTHREAD; pre++) {
-        if (coroutine[pre].used == false) {
+    for (pre = 0; pre < MAX_CO; pre++) {
+        if (coroutine[pre].state == COROUTINE_DEAD) {
             break;
         }
     }
-    if (pre == MAXTHREAD) {
+    if (pre == MAX_CO) {
         printf("NO ENOUGH THREADS!\n");
         assert(0);
     }
@@ -45,7 +49,7 @@ struct co* co_start(const char *name, func_t func, void *arg) {
     strcpy(coroutine[pre].name, name);
     // coroutine[co_cnt].name = name;
     coroutine[pre].func = func;
-    coroutine[pre].used = true;
+    coroutine[pre].state = COROUTINE_READY;
 
     if (tos == NULL) {
         tos = (void *)&arg;
@@ -54,40 +58,54 @@ struct co* co_start(const char *name, func_t func, void *arg) {
     char arg_n[STACKDIR(tos - (void *)&arg)];
     coroutine[pre].coarg = arg_n;
 
-    current = pre;
+    current = &coroutine[pre];
 
-    if (setjmp(coroutine[pre].env)) {
-        return (struct co*)(coroutine + pre);
+    if (setjmp(coroutine[pre].buf)) {
+        return current;
     }
     /* else {
-        func(arg);
-    }
-    */
+       func(arg);
+       }
+       */
     // func(arg); // Test #2 hangs
     return (struct co*)(coroutine + pre);
 }
 
 void co_yield() {
-    int go;
-    for (go = 0; go < MAXTHREAD; go++) {
-        if (coroutine[go].used == true) {
-            break;
+    int val = setjmp(current->buf);
+    if (val == 0) {
+        int go;
+        for (go = 0; go < MAX_CO; go++) {
+            if (coroutine[go].state != COROUTINE_DEAD
+                    && coroutine[go].state != COROUTINE_RUNNING) {
+                break;
+            }
         }
-    }
-    if (go == MAXTHREAD) {
-        printf("NO ACCESSIBLE COROUTINE!\n");
+        if (go == MAX_CO) {
+            printf("NO ACCESSIBLE COROUTINE!\n");
+            return;
+        }
+        longjmp(coroutine[go].buf, 1);
+    } else {
         return;
     }
-
-    if (setjmp(coroutine[current].env)) {
-        return;
-    }
-
-    current = go;
-    longjmp(coroutine[go].env, 1);
 }
 
 void co_wait(struct co *thd) {
+    if (thd == NULL) {
+        return;
+    }
+
+    int state = thd->state;
+    switch(state) {
+        case COROUTINE_READY:
+
+
+        default:
+            printf("Wrong State!\n");
+            assert(0);
+    }
+
     memset(thd, 0, sizeof(struct co));
     return;
 }
