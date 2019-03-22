74b3092b2a39896192b47dba3b96191d7beba383
tracer-nju <tracer@nju.edu.cn>
2019-03-22 11:05:37 +0800

    Static hostname: Inspiron          Icon name: computer-laptop            Chassis: laptop         Machine ID: 537ec2aa89e64954b7d607f1de70fb4e            Boot ID: b0bbf33baaf441e294b404a58189d5d3   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.15.0-47-generic       Architecture: x86-64  11:05:37 up 2 days, 14:52,  8 users,  load average: 0.50, 0.48, 0.73

--- libco/co.c
+++ libco/co.c
@@ -66,8 +66,8 @@ struct co* co_start(const char *name, func_t func, void *arg) {
     coroutine[pre].state = COROUTINE_READY;
 
     if (setjmp(coroutine[pre].buf)) {
-        printf("***\n");
         if (coroutine[pre].stack == NULL) {
+        printf("***\n");
             coroutine[pre].stack = malloc(STACKSIZE);
             coroutine[pre].stack -= STACKSIZE;
         }
