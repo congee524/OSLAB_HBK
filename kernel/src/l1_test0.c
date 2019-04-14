e1012cc5a084db185be0901812f2111e6e6576fe
tracer-nju <tracer@nju.edu.cn>
2019-04-14 15:45:58 +0800

    Static hostname: Inspiron          Icon name: computer-laptop            Chassis: laptop         Machine ID: 537ec2aa89e64954b7d607f1de70fb4e            Boot ID: a62d35a262694d39877ca641b831fbb0   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.15.0-47-generic       Architecture: x86-64  15:45:58 up  6:32,  3 users,  load average: 1.19, 1.39, 1.31

---
+++ kernel/src/l1_test0.c
@@ -0,0 +1,14 @@
+#include <kernel.h>
+
+static void* s[4][11451] = {};
+static void test() {
+  srand(uptime());
+  for (int i = 0; i < 11451; ++i) {
+    s[_cpu()][i] = pmm->alloc(rand() % (1024));
+  }
+  for (int i = 0; i < 11451; ++i) {
+    pmm->free(s[_cpu()][i]);
+  }
+
+  printf("SUCCESS ON CPU %d", _cpu());
+}
