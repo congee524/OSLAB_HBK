9ad219d8aff8c8575819e847b3f8f497581bdd83
tracer-nju <tracer@nju.edu.cn>
2019-04-22 10:59:13 +0800

    Static hostname: Inspiron          Icon name: computer-laptop            Chassis: laptop         Machine ID: 537ec2aa89e64954b7d607f1de70fb4e            Boot ID: 2c5b05c70eab4f12a4dad5d8dc4de824   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.15.0-47-generic       Architecture: x86-64  10:59:13 up 53 min,  3 users,  load average: 0.64, 0.67, 0.81

--- sperf/sperf.c
+++ sperf/sperf.c
@@ -1,8 +1,48 @@
 #include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
 
-int main(int argc, char *argv[]) {
+char buffer[1024];
+
+int main(int argc, char* argv[]) {
   for (int i = 0; i < argc; i++) {
     printf("argv[%d]: %s\n", i, argv[i]);
   }
+  if (argc <= 1) {
+    printf("please input parameters!\n");
+    return 0;
+  }
+
+  int status;
+
+  int pfd[2];
+  if (pipe(pfd) < 0) {
+    return -1;
+  }
+
+  char st_argv[argc + 2];
+  st_argv[0] = "strace";
+  st_argv[1] = "-T";
+  for (int i = 1; i < argc; i++) {
+    st_argv[i + 1] = argv[i];
+  }
+
+  pid_t pid = fork();
+  if (pid == 0) {
+    dup2(pfd[1], STDOUT_FILENO);
+    close(pfd[0]);
+    execvp("strace", st_argv);
+    exit(0);
+  } else {
+    close(pfd[1]);
+    int len;
+    while ((len = read(pfd[0], buffer, 1023)) > 0) {
+      buffer[len] = '\0';
+      printf("%s\n", buffer);
+    }
+
+    waitpid((pid_t)pid, &status, 0);
+  }
+
   return 0;
 }
