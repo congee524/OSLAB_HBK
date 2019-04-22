ba14af1ab55996be1e71d425c28f7eeb98e6d614
tracer-nju <tracer@nju.edu.cn>
2019-04-22 20:26:48 +0800

    Static hostname: Inspiron          Icon name: computer-laptop            Chassis: laptop         Machine ID: 537ec2aa89e64954b7d607f1de70fb4e            Boot ID: 16b167dbfe48420da1be826175b9e07b   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.15.0-47-generic       Architecture: x86-64  20:26:48 up  1:29,  2 users,  load average: 0.67, 0.80, 0.96

--- sperf/sperf.c
+++ sperf/sperf.c
@@ -6,6 +6,15 @@
 #include <sys/wait.h>
 #include <unistd.h>
 
+#define MAX_FUNC 256
+
+typedef struct {
+  char name[32];
+  double t;
+} f_t;
+
+f_t func_time[MAX_FUNC];
+
 int main(int argc, char* argv[]) {
   /*
   for (int i = 0; i < argc; i++) {
@@ -37,6 +46,7 @@ int main(int argc, char* argv[]) {
   if (pid == 0) {
     dup2(fildes[1], STDERR_FILENO);
     close(fildes[0]);
+    close(STDOUT_FILENO);
     execvp("strace", st_argv);
     exit(0);
   } else {
@@ -47,11 +57,16 @@ int main(int argc, char* argv[]) {
     char* pat_func = "^[a-zA-Z0-9_]{2,}";
     char* pat_time = "<[0-9.]{2,}>";
     char errbuf[1024];
-    char match[100];
+    char match_func[32];
+    char match_time[32];
     regex_t reg_func, reg_time;
     int err = 0, nm = 1;
     regmatch_t pmatch_func[nm];
     regmatch_t pmatch_time[nm];
+
+    int cnt = 0;
+    char M_time[32];
+    double tt;
 
     if (regcomp(&reg_func, pat_func, REG_EXTENDED) < 0) {
       regerror(err, &reg_func, errbuf, sizeof(errbuf));
@@ -67,6 +82,7 @@ int main(int argc, char* argv[]) {
     while (fgets(buffer, 1023, stdin)) {
       // printf("%s", buffer);
 
+      // String Matching
       err = regexec(&reg_func, buffer, nm, pmatch_func, 0);
       if (err == REG_NOMATCH) {
         printf("no match\n");
@@ -91,9 +107,9 @@ int main(int argc, char* argv[]) {
         int len = pmatch_func[i].rm_eo - pmatch_func[i].rm_so;
         // printf("\nlen: %d\n", len);
         if (len) {
-          memset(match, '\0', sizeof(match));
-          memcpy(match, buffer + pmatch_func[i].rm_so, len);
-          printf("%s\n", match);
+          memset(match_func, '\0', sizeof(match_func));
+          memcpy(match_func, buffer + pmatch_func[i].rm_so, len);
+          // printf("%s\n", match_func);
         }
       }
 
@@ -101,22 +117,42 @@ int main(int argc, char* argv[]) {
         int len = pmatch_time[i].rm_eo - pmatch_time[i].rm_so;
         // printf("\nlen: %d\n", len);
         if (len) {
-          memset(match, '\0', sizeof(match));
-          memcpy(match, buffer + pmatch_time[i].rm_so, len);
-          printf("%s\n", match);
+          memset(match_time, '\0', sizeof(match_time));
+          memcpy(match_time, buffer + pmatch_time[i].rm_so, len);
+          // printf("%s\n", match_time);
         }
+      }
+
+      // handle the string
+      memset(M_time, '\0', sizeof(M_time));
+      for (int i = 1; i < 32; i++) {
+        if (match_time[i] == '>') {
+          break;
+        }
+        M_time[i - 1] = match_time[i];
+      }
+      tt = atof(M_time);
+      int flag = 0;
+      for (int i = 0; i < cnt; i++) {
+        if (strcmp(func_time[i].name, match_func) == 0) {
+          func_time[i].t += tt;
+          flag = 1;
+          break;
+        }
+      }
+      if (!flag) {
+        strcpy(func_time[cnt].name, match_func);
+        func_time[cnt].tt = 0;
+        cnt++;
+        assert(cnt <= MAX_FUNC);
       }
     }
 
-    /*
-    char buffer[1024] = {0};
-    int len;
-    while ((len = read(fildes[0], buffer, 1023) > 0)) {
-      printf("%s\n\n", buffer);
-      memset(buffer, '\0', 1024);
-    }
-    */
     waitpid((pid_t)pid, &status, 0);
+
+    for (int i = 0; i < cnt; i++) {
+      printf("%s: %lf\n", func_time[i].name, func_time[i].t);
+    }
   }
 
   return 0;
