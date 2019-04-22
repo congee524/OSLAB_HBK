ed3c600d13482082c85b1fa29bb03efe6942d316
tracer-nju <tracer@nju.edu.cn>
2019-04-22 21:45:37 +0800

    Static hostname: Inspiron          Icon name: computer-laptop            Chassis: laptop         Machine ID: 537ec2aa89e64954b7d607f1de70fb4e            Boot ID: 16b167dbfe48420da1be826175b9e07b   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.15.0-47-generic       Architecture: x86-64  21:45:37 up  2:47,  2 users,  load average: 1.11, 1.18, 1.12

--- sperf/sperf.c
+++ sperf/sperf.c
@@ -159,7 +159,7 @@ int main(int argc, char* argv[]) {
         */
 
     for (int i = 0; i < cnt; i++) {
-      printf("%s: %lf\n", func_time[i].name, func_time[i].t * 100 / tot_tt);
+      printf("%s: %lf%%\n", func_time[i].name, func_time[i].t * 100 / tot_tt);
     }
   }
   waitpid((pid_t)pid, &status, 0);
