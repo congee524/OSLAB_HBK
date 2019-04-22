a4a95c224cd5b03ebfef7cfa441f6e126698f422
tracer-nju <tracer@nju.edu.cn>
2019-04-22 19:46:21 +0800

    Static hostname: Inspiron          Icon name: computer-laptop            Chassis: laptop         Machine ID: 537ec2aa89e64954b7d607f1de70fb4e            Boot ID: 16b167dbfe48420da1be826175b9e07b   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.15.0-47-generic       Architecture: x86-64  19:46:21 up 48 min,  2 users,  load average: 0.72, 0.84, 0.88

--- sperf/sperf.c
+++ sperf/sperf.c
@@ -38,11 +38,11 @@ int main(int argc, char* argv[]) {
     dup2(STDERR_FILENO, fildes[1]);
     close(fildes[0]);
     close(STDOUT_FILENO);
+    close(STDERR_FILENO);
     execvp("strace", st_argv);
     exit(0);
   } else {
     dup2(fildes[0], STDIN_FILENO);
-
     close(fildes[1]);
     /*
         char* pat_func = "^[a-zA-Z0-9_]{2,}";
