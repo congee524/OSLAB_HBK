9cae76d6a43ee596b1ec09e88dee983497fe4f41
tracer-nju <tracer@nju.edu.cn>
2019-05-09 15:07:45 +0800

    Static hostname: Inspiron          Icon name: computer-laptop            Chassis: laptop         Machine ID: 537ec2aa89e64954b7d607f1de70fb4e            Boot ID: a55fb866c2ec48ca87b765a076bed7ae   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.15.0-47-generic       Architecture: x86-64  15:07:45 up 51 min,  4 users,  load average: 1.17, 0.81, 0.62

--- kernel/src/kmt.c
+++ kernel/src/kmt.c
@@ -43,6 +43,7 @@ MODULE_DEF(kmt) {
     .init = kmt_init,
     .create = kmt_create,
     .teardown = kmt_teardown,
+    .spin_init = kmt_spin_init,
     .spin_lock = kmt_spin_lock,
     .spin_unlock = kmt_spin_unlock,
     .sem_init = kmt_sem_init,
