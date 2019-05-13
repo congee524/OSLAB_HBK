cff8d7884afb4895bc7470002bc42299d36c1243
tracer-nju <tracer@nju.edu.cn>
2019-05-13 10:34:49 +0800

    Static hostname: Inspiron          Icon name: computer-laptop            Chassis: laptop         Machine ID: 537ec2aa89e64954b7d607f1de70fb4e            Boot ID: 320d74143a9e463a89d4a18be1980d7b   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.15.0-49-generic       Architecture: x86-64  10:34:49 up 11:48,  1 user,  load average: 1.84, 1.89, 1.77

--- kernel/src/os.c
+++ kernel/src/os.c
@@ -8,11 +8,11 @@ void echo_task(void *arg) {
   while (1) {
     char line[128], text[128];
     sprintf(text, "(%s) $ ", name);
-    tty->ops->write(tty, text);
+    tty->ops->write(tty, 0, text, strlen(text));
     int nread = tty->ops->read(tty, 0, line, sizeof(line));
     line[nread - 1] = '\0';
     sprintf(text, "Echo: %s.\n", line);
-    tty->ops->write(tty, text);
+    tty->ops->write(tty, 0, text, strlen(text));
   }
 }
 
