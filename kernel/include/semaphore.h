c73dfc67ef848eae0c74af294cb145525a915136
tracer-nju <tracer@nju.edu.cn>
2019-05-14 16:34:13 +0800

    Static hostname: Inspiron          Icon name: computer-laptop            Chassis: laptop         Machine ID: 537ec2aa89e64954b7d607f1de70fb4e            Boot ID: befd52e2790f493780c0832d19c74dff   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.15.0-49-generic       Architecture: x86-64  16:34:14 up 21:18,  1 user,  load average: 1.27, 1.76, 1.83

---
+++ kernel/include/semaphore.h
@@ -0,0 +1,52 @@
+#include <am.h>
+#include <kernel.h>
+
+spinlock_t sleep_lk;
+
+void sleep(task_t *chan, spinlock_t *lk) {
+  if (!current) panic("sleep");
+
+  if (!lk) panic("sleep without lk");
+
+  if (lk != &sleep_lk) {
+    kmt->spin_lock(&sleep_lk);
+    kmt->spin_unlock(lk);
+  }
+
+  chan->state = SLEEPING;
+  _yield();
+
+  if (lk != &sleep_lk) {
+    kmt->spin_unlock(&sleep_lk);
+    kmt->spin_lock(lk);
+  }
+}
+
+void wakeupl(task_t *chan) {
+  task_t *tmp;
+  for (int i = 0; i < _ncpu(); i++) {
+    if (tasks[i].cnt > 0) {
+      tmp = tasks[i].head;
+      if (tmp->status == SLEEPING && strcmp(tmp->name, chan->name) == 0) {
+        tmp->status = RUNNABLE;
+        flag = 1;
+        break;
+      }
+      while (tmp->next) {
+        tmp = tmp->next;
+        if (tmp->status == SLEEPING && strcmp(tmp->name, chan->name) == 0) {
+          tmp->status = RUNNABLE;
+          flag = 1;
+          break;
+        }
+      }
+    }
+    if (flag) break;
+  }
+}
+
+void wakeup(task_t *chan) {
+  kmt->spin_lock(&sleep_lk);
+  wakeupl(chan);
+  kmt->spin_unlock(&sleep_lk);
+}
