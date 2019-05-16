10f469236db6ac2f9e9097ab72cabe95d046a7da
tracer-nju <tracer@nju.edu.cn>
2019-05-16 20:49:05 +0800

    Static hostname: Inspiron          Icon name: computer-laptop            Chassis: laptop         Machine ID: 537ec2aa89e64954b7d607f1de70fb4e            Boot ID: 65e57f8990084104b3fc458d8bfd43ea   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.15.0-50-generic       Architecture: x86-64  20:49:05 up  7:56,  1 user,  load average: 1.80, 1.69, 1.26

--- kernel/src/kmt.c
+++ kernel/src/kmt.c
@@ -188,8 +188,7 @@ int holding(spinlock_t *lock) { return lock->locked && lock->cpu == _cpu(); }
 // it takes two popcli to undo two pushcli.  Also, if interrupts
 // are off, then pushcli, popcli leaves them off
 void pushcli(void) {
-  int eflags;
-
+  uint eflags;
   eflags = readeflags();
   cli();
   if (ncli[_cpu()]++ == 0) intena[_cpu()] = eflags & FL_IF;
