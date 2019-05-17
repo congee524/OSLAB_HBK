c99243174da6dc25a727915eff3d8fc57c003081
tracer-nju <tracer@nju.edu.cn>
2019-05-17 15:57:41 +0800

    Static hostname: Inspiron          Icon name: computer-laptop            Chassis: laptop         Machine ID: 537ec2aa89e64954b7d607f1de70fb4e            Boot ID: 86b885f391ec48c096af1e649357897b   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.15.0-50-generic       Architecture: x86-64  15:57:41 up 10 min,  1 user,  load average: 1.15, 1.07, 0.67

--- kernel/src/os.c
+++ kernel/src/os.c
@@ -61,7 +61,7 @@ static void os_run() {
 static _Context *os_trap(_Event ev, _Context *ctx) {
   // kmt->spin_lock(&os_trap_lk);
   _Context *ret = NULL;
-  for (int i = 0; i < MAX_HANDLER; i++) {
+  for (int i = 0; i < cnt_handle; i++) {
     if (handlers[i].event == _EVENT_NULL || handlers[i].event == ev.event) {
       _Context *next = handlers[i].handler(ev, ctx);
       if (next) ret = next;
