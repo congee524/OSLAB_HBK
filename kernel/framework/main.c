00391cf656f4d0d9ec69cf85fb87af82a6b86c5b
tracer-nju <tracer@nju.edu.cn>
2019-05-17 16:20:40 +0800

    Static hostname: Inspiron          Icon name: computer-laptop            Chassis: laptop         Machine ID: 537ec2aa89e64954b7d607f1de70fb4e            Boot ID: 86b885f391ec48c096af1e649357897b   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.15.0-50-generic       Architecture: x86-64  16:20:41 up 33 min,  1 user,  load average: 0.75, 1.00, 0.97

--- kernel/framework/main.c
+++ kernel/framework/main.c
@@ -4,9 +4,9 @@
 
 static void echo_task(void *arg) {
   char *name = (char *)arg;
+  char line[128] = "", text[128] = "";
   device_t *tty = dev_lookup(name);
   while (1) {
-    char line[128], text[128];
     sprintf(text, "(%s) $ ", name);
     tty->ops->write(tty, 0, text, strlen(text));
     int nread = tty->ops->read(tty, 0, line, sizeof(line));
