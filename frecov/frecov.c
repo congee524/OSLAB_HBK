eb93d32997bf15abbfbd15d393eb23e49b0019cc
tracer-nju <tracer@nju.edu.cn>
2019-05-27 15:47:27 +0800

    Static hostname: Inspiron          Icon name: computer-laptop            Chassis: laptop         Machine ID: 537ec2aa89e64954b7d607f1de70fb4e            Boot ID: ee33fc93ab8e4674842bfcfd31edda4b   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.15.0-51-generic       Architecture: x86-64  15:47:27 up 15:07,  3 users,  load average: 2.00, 2.25, 2.23

--- frecov/frecov.c
+++ frecov/frecov.c
@@ -62,16 +62,16 @@ typedef struct {
 
 typedef struct {
   BYTE SequeNumber;  // 0x00 序列号
-  // wchar_t name1[5];  // 0x01 文件名的第1-5个Unicode码字符
-  char name1[10];
-  BYTE Attr;       // 0x0b 属性标志 0xOF固定值
-  BYTE Reserved1;  // 0x0c 保留未用
-  BYTE CheckSum;   // 0x0d 短文件名检验和
-  // wchar_t name2[6];  // 0x0e 文件名的第6-11个Unicode码字符
-  char name2[12];
-  WORD Reserved2;  // 0x1A 保留未用 始终为0
-  // wchar_t name3[2];  // 0x1c 文件名的第12-13个Unicode码字符
-  char name3[4];
+  wchar_t name1[5];  // 0x01 文件名的第1-5个Unicode码字符
+  // char name1[10];
+  BYTE Attr;         // 0x0b 属性标志 0xOF固定值
+  BYTE Reserved1;    // 0x0c 保留未用
+  BYTE CheckSum;     // 0x0d 短文件名检验和
+  wchar_t name2[6];  // 0x0e 文件名的第6-11个Unicode码字符
+  // char name2[12];
+  WORD Reserved2;    // 0x1A 保留未用 始终为0
+  wchar_t name3[2];  // 0x1c 文件名的第12-13个Unicode码字符
+  // char name3[4];
 } __attribute__((__packed__)) LFNEntry;
 
 int main(int argc, char *argv[]) {
@@ -123,9 +123,9 @@ int main(int argc, char *argv[]) {
     for (int pos = data_SecNum * bps; pos < sb.st_size; pos += 32) {
       LFNEntry *LFN = (LFNEntry *)(addr + pos);
       if (LFN->Attr == 0x0f && LFN->SequeNumber == 0xe5) {
-        for (int i = 0; i < 10; i++) printf("%c", LFN->name1[i]);
-        for (int i = 0; i < 12; i++) printf("%c", LFN->name2[i]);
-        for (int i = 0; i < 4; i++) printf("%c", LFN->name3[i]);
+        for (int i = 0; i < 5; i++) printf("%lc", LFN->name1[i]);
+        for (int i = 0; i < 6; i++) printf("%lc", LFN->name2[i]);
+        for (int i = 0; i < 2; i++) printf("%lc", LFN->name3[i]);
       }
     }
   }
