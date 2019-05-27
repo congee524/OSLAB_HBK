fa215216b754c7fbb27c30dfb0af57d3bb539e0f
tracer-nju <tracer@nju.edu.cn>
2019-05-27 20:47:53 +0800

    Static hostname: Inspiron          Icon name: computer-laptop            Chassis: laptop         Machine ID: 537ec2aa89e64954b7d607f1de70fb4e            Boot ID: ee33fc93ab8e4674842bfcfd31edda4b   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.15.0-51-generic       Architecture: x86-64  20:47:53 up 20:08,  3 users,  load average: 2.27, 1.98, 1.72

--- frecov/frecov.c
+++ frecov/frecov.c
@@ -82,18 +82,15 @@ char *trname(LFNEntry *LFN, char *nbuffer) {
   char tmp_na;
   for (int i = 1; i >= 0; i--) {
     tmp_na = (char)LFN->name3[i];
-    // if (tmp_na >= 0x20 && tmp_na <= 0x7e) *(--nbuffer) = tmp_na;
-    if (tmp_na) *(--nbuffer) = tmp_na;
+    if (tmp_na >= 0x30 && tmp_na <= 0x7e) *(--nbuffer) = tmp_na;
   }
   for (int i = 5; i >= 0; i--) {
     tmp_na = (char)LFN->name2[i];
-    // if (tmp_na >= 0x20 && tmp_na <= 0x7e) *(--nbuffer) = tmp_na;
-    if (tmp_na) *(--nbuffer) = tmp_na;
+    if (tmp_na >= 0x30 && tmp_na <= 0x7e) *(--nbuffer) = tmp_na;
   }
   for (int i = 4; i >= 0; i--) {
     tmp_na = (char)LFN->name1[i];
-    // if (tmp_na >= 0x20 && tmp_na <= 0x7e) *(--nbuffer) = tmp_na;
-    if (tmp_na) *(--nbuffer) = tmp_na;
+    if (tmp_na >= 0x30 && tmp_na <= 0x7e) *(--nbuffer) = tmp_na;
   }
   return nbuffer;
 }
