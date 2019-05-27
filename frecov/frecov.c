c81fe51d82c8c29fd7510f27c4a32e9fb585abde
tracer-nju <tracer@nju.edu.cn>
2019-05-27 15:32:44 +0800

    Static hostname: Inspiron          Icon name: computer-laptop            Chassis: laptop         Machine ID: 537ec2aa89e64954b7d607f1de70fb4e            Boot ID: ee33fc93ab8e4674842bfcfd31edda4b   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.15.0-51-generic       Architecture: x86-64  15:32:44 up 14:52,  3 users,  load average: 1.87, 2.25, 2.11

--- frecov/frecov.c
+++ frecov/frecov.c
@@ -120,10 +120,9 @@ int main(int argc, char *argv[]) {
     for (int pos = data_SecNum * bps; pos < sb.st_size; pos += 32) {
       LFNEntry *LFN = (LFNEntry *)(addr + pos);
       if (LFN->Attr == 0x0f) {
-        printf("%ls", LFN->name1);
-        printf("%ls", LFN->name2);
-        printf("%ls", LFN->name3);
-        printf("\n");
+        printf("%s", LFN->name1);
+        printf("%s", LFN->name2);
+        printf("%s", LFN->name3);
       }
     }
   }
