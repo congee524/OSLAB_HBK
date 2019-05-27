378962445d37e19196821d223b0079a1fe88d971
tracer-nju <tracer@nju.edu.cn>
2019-05-27 19:14:44 +0800

    Static hostname: Inspiron          Icon name: computer-laptop            Chassis: laptop         Machine ID: 537ec2aa89e64954b7d607f1de70fb4e            Boot ID: ee33fc93ab8e4674842bfcfd31edda4b   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.15.0-51-generic       Architecture: x86-64  19:14:44 up 18:35,  3 users,  load average: 0.89, 1.03, 1.16

--- frecov/frecov.c
+++ frecov/frecov.c
@@ -1,4 +1,80 @@
-#include <frecov.h>
+#include <fcntl.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <wchar.h>
+
+#define DOS_DIR_SIZE 512
+
+typedef uint8_t BYTE;
+typedef uint16_t WORD;
+typedef uint32_t DWORD;
+
+#define LASTDIR(num) ((num >> 6) & 1)
+
+typedef struct {
+  BYTE BS_jmpBoot[3];             // 0X00 跳跃指令
+  BYTE BS_OEMName[8];             // 0X03 OEM代码
+  WORD BPBBytesPerSector;         // 0x0B 每扇区字节数
+  BYTE BPBSectorsPerClusters;     // 0x0D 每簇扇区数
+  WORD BPBReservedSectorCount;    // 0x0E 保留扇区数
+  BYTE BPBNumberOfFATs;           // 0x10 FAT表个数
+  WORD BPBFAT32Unused1;           // 0x11 未用
+  WORD BPBFAT32Unused2;           // 0x13 未用
+  BYTE BPBMedia;                  // 0x15 介质描述符
+  WORD BPBFAT32Unused3;           // 0x16 未用
+  WORD BPBSectorPerTruck;         // 0x18 每磁道扇区数
+  WORD BPBNumberOfHeads;          // 0x1A 磁头数
+  DWORD BPBHideSector;            // 0x1C 隐藏扇区
+  DWORD BPBTotalSector;           // 0x20 该分区总扇区数
+  DWORD PBPSectorPerFAT;          // 0x24 每FAT扇区数
+  WORD BPBExtFlag;                // 0x28 标记
+  WORD BPBRevesion;               // 0x2A 版本
+  DWORD BPBRootDirectoryCluster;  // 0x2C 根目录首簇号
+  WORD BPBFSInfo;                 // 0x30 文件系统信息扇区号
+  WORD BPBBackupDBRSector;        // 0x32 DBR备份扇区号
+  BYTE BPBReserved[12];           // 0x34 保留
+  BYTE BPBBIOSDriveNumber;        // 0x40 BIOS驱动器号
+  BYTE BPBFAT32Unused4;           // 0x41 未用
+  BYTE BPBExBootFlag;             // 0x42 扩展引导标记
+  DWORD BPBVolumeID;              // 0x43 卷序列号
+  BYTE BPBVolumeName[11];         // 0x47 卷标
+  BYTE BPBFileSystem[8];          // 0x52 文件系统类型
+} __attribute__((__packed__)) BootEntry;
+
+typedef struct {
+  char Name[8];               // 0x00 主文件名
+  char ExtendName[3];         // 0x08 扩展名
+  BYTE Attribute;             // 0x0B 文件属性
+  BYTE Reserved;              // 0x0C 未用
+  BYTE FileCreateTimeSecond;  // 0x0D 文件创建时间精确到秒
+  WORD FileCreateTime;        // 0x0E 文件创建时间
+  WORD FileCreateDate;        // 0x10 文件创建日期
+  WORD FileAccessDate;        // 0x12 文件访问日期
+  WORD FileStartClusterHigh;  // 0x14 文件起始簇号高位
+  WORD FileModifyTime;        // 0x16 修改时间
+  WORD FileModifyDate;        // 0x18 修改日期
+  WORD FileStartClusterLow;   // 0x1A 文件起始簇号低位
+  DWORD FileSize;             // 0x1C 文件大小（Byte为单位）
+} __attribute__((__packed__)) DirEntry;
+
+typedef struct {
+  BYTE SequeNumber;  // 0x00 序列号
+  // wchar_t name1[5];  // 0x01 文件名的第1-5个Unicode码字符
+  char name1[10];
+  BYTE Attr;       // 0x0b 属性标志 0xOF固定值
+  BYTE Reserved1;  // 0x0c 保留未用
+  BYTE CheckSum;   // 0x0d 短文件名检验和
+  // wchar_t name2[6];  // 0x0e 文件名的第6-11个Unicode码字符
+  char name2[12];
+  WORD Reserved2;  // 0x1A 保留未用 始终为0
+  // wchar_t name3[2];  // 0x1c 文件名的第12-13个Unicode码字符
+  char name3[4];
+} __attribute__((__packed__)) LFNEntry;
 
 int main(int argc, char *argv[]) {
   if (argc <= 1) {
