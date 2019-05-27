9b7268c619f27c1a4e095b30656ac5f69c3f6e39
tracer-nju <tracer@nju.edu.cn>
2019-05-27 11:24:43 +0800

    Static hostname: Inspiron          Icon name: computer-laptop            Chassis: laptop         Machine ID: 537ec2aa89e64954b7d607f1de70fb4e            Boot ID: ee33fc93ab8e4674842bfcfd31edda4b   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.15.0-51-generic       Architecture: x86-64  11:24:43 up 10:44,  3 users,  load average: 1.89, 1.08, 1.07

--- frecov/frecov.c
+++ frecov/frecov.c
@@ -1,51 +1,113 @@
+#include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <unistd.h>
 
-typedef struct BootEntry {
-  // __attribute__((packed)) means align according to the actual bytes occupied
-  unsigned char BS_jmpBoot[3] __attribute__((packed));
-  unsigned char BS_OEMName[8] __attribute__((packed));
-  unsigned short BPB_BytsPerSec __attribute__((packed));
-  unsigned char BPB_SecPerClus __attribute__((packed));
-  unsigned short BPB_RsvdSecCnt __attribute__((packed));
-  unsigned char BPB_NumFATs __attribute__((packed));
-  unsigned short BPB_RootEntCnt __attribute__((packed));
-  unsigned short BPB_TotSec16 __attribute__((packed));
-  unsigned char BPB_Media __attribute__((packed));
-  unsigned short BPB_FATSz16 __attribute__((packed));
-  unsigned short BPB_SecPerTrk __attribute__((packed));
-  unsigned short BPB_NumHeads __attribute__((packed));
-  unsigned long BPB_HiddSec __attribute__((packed));
-  unsigned long BPB_TotSec32 __attribute__((packed));
-  unsigned long BPB_FATSz32 __attribute__((packed));
-  unsigned short BPB_ExtFlags __attribute__((packed));
-  unsigned short BPB_FSVer __attribute__((packed));
-  unsigned long BPB_RootClus __attribute__((packed));
-  unsigned short BPB_FSInfo __attribute__((packed));
-  unsigned short BPB_BkBootSec __attribute__((packed));
-  unsigned char BPB_Reserved[12] __attribute__((packed));
-  unsigned char BS_DrvNum __attribute__((packed));
-  unsigned char BS_Reserved1 __attribute__((packed));
-  unsigned char BS_BootSig __attribute__((packed));
-  unsigned long BS_VolID __attribute__((packed));
-  unsigned char BS_VolLab[11] __attribute__((packed));
-  unsigned char BS_FilSysType[8] __attribute__((packed));
-} BootEntry;
+#define DOS_DIR_SIZE 512
 
-typedef struct DirEntry {
-  unsigned char DIR_Name[11] __attribute__((packed));
-  unsigned char DIR_Attr __attribute__((packed));
-  unsigned char DIR_NTRes __attribute__((packed));
-  unsigned char DIR_CrtTimeTenth __attribute__((packed));
-  unsigned short DIR_CrtTime __attribute__((packed));
-  unsigned short DIR_CrtDate __attribute__((packed));
-  unsigned short DIR_LstAccDate __attribute__((packed));
-  unsigned short DIR_FstClusHI __attribute__((packed));
-  unsigned short DIR_WrtTime __attribute__((packed));
-  unsigned short DIR_WrtDate __attribute__((packed));
-  unsigned short DIR_FstClusLO __attribute__((packed));
-  unsigned long DIR_FileSize __attribute__((packed));
-} DirEntry;
+typedef unsigned char BYTE;
+typedef unsigned short WORD;
+typedef unsigned long DWORD;
+
+typedef struct {
+  BYTE BS_jmpBoot[3];             // 0X00 跳跃指令
+  BYTE BS_OEMName[8];             // 0X03 OEM代码
+  WORD BPBBytesPerSector;         // 0x0B 每扇区字节数
+  BYTE BPBSectorsPerClusters;     // 0x0D 每簇扇区数
+  WORD BPBReservedSectorCount;    // 0x0E 保留扇区数
+  BYTE BPBNumberOfFATs;           // 0x10 FAT表个数
+  WORD BPBFAT32Unused1;           // 0x11 未用
+  WORD BPBFAT32Unused2;           // 0x13 未用
+  BYTE BPBMedia;                  // 0x15 介质描述符
+  WORD BPBFAT32Unused3;           // 0x16 未用
+  WORD BPBSectorPerTruck;         // 0x18 每磁道扇区数
+  WORD BPBNumberOfHeads;          // 0x1A 磁头数
+  DWORD BPBHideSector;            // 0x1C 隐藏扇区
+  DWORD BPBTotalSector;           // 0x20 该分区总扇区数
+  DWORD PBPSectorPerFAT;          // 0x24 每FAT扇区数
+  WORD BPBExtFlag;                // 0x28 标记
+  WORD BPBRevesion;               // 0x2A 版本
+  DWORD BPBRootDirectoryCluster;  // 0x2C 根目录首簇号
+  WORD BPBFSInfo;                 // 0x30 文件系统信息扇区号
+  WORD BPBBackupDBRSector;        // 0x32 DBR备份扇区号
+  BYTE BPBReserved[12];           // 0x34 保留
+  BYTE BPBBIOSDriveNumber;        // 0x40 BIOS驱动器号
+  BYTE BPBFAT32Unused4;           // 0x41 未用
+  BYTE BPBExBootFlag;             // 0x42 扩展引导标记
+  DWORD BPBVolumeID;              // 0x43 卷序列号
+  BYTE BPBVolumeName[11];         // 0x47 卷标
+  BYTE BPBFileSystem[8];          // 0x52 文件系统类型
+} __attribute__((__packed__)) BootEntry;
+
+typedef struct {
+  char Name[8];               // 0x00 主文件名
+  char ExtendName[3];         // 0x08 扩展名
+  BYTE Attribute;             // 0x0B 文件属性
+  BYTE Reserved;              // 0x0C 未用
+  BYTE FileCreateTimeSecond;  // 0x0D 文件创建时间精确到秒
+  WORD FileCreateTime;        // 0x0E 文件创建时间
+  WORD FileCreateDate;        // 0x10 文件创建日期
+  WORD FileAccessDate;        // 0x12 文件访问日期
+  WORD FileStartClusterHigh;  // 0x14 文件起始簇号高位
+  WORD FileModifyTime;        // 0x16 修改时间
+  WORD FileModifyDate;        // 0x18 修改日期
+  WORD FileStartClusterLow;   // 0x1A 文件起始簇号低位
+  DWORD FileSize;             // 0x1C 文件大小（Byte为单位）
+} __attribute__((__packed__)) DirEntry;
+
+int main(int argc, char *argv[]) {
+  if (argc <= 1) {
+    printf("PLEASE INPUT FILENAME!\n");
+    return 0;
+  }
+  int ca = 1;
+  while (ca < argc) {
+    BootEntry bootEntry;
+    DirEntry DirEntry;
+    char *addr;
+    int fd;
+    struct stat sb;
+    size_t length;
+    ssize_t s;
+
+    char *filename = argv[ca++];
+    fd = open(filename, O_RDONLY);
+    if ((fd = open(filename, O_RDONLY)) == -1) {
+      printf("Wrong filename %s!\n", filename);
+      return 1;
+    }
+    if (fstat(fd, &sb) == -1) {
+      printf("fstat go wrong!\n");
+      return 1;
+    }
+    addr = mmap(NULL, sb.st_size, PROT_READ, MAP_PRIVATE, fd, 0);
+    if (addr == MAP_FAILED) {
+      printf("mmap go wrong!\n");
+      return 1;
+    }
+    // read DBR, get BPB parameters
+    memcpy(&bootEntry, addr, sizeof(bootEntry));
+    int bps = bootEntry.BPBBytesPerSector;      // 每扇区字节
+    int spc = bootEntry.BPBSectorsPerClusters;  // 每簇扇区
+    printf("Name = %s\n", bootEntry.BS_OEMName);
+    printf("Bytes per Sector = %d\n", bps);
+    printf("Sector per Cluster = %d\n", spc);
+
+    printf("Reserved Sector Count = %d\n", bootEntry.BPBReservedSectorCount);
+    printf("Number of FATs = %d\n", bootEntry.BPBNumberOfFATs);
+    printf("Number of FAT sectors = %ld\n", bootEntry.PBPSectorPerFAT);
+    int FAT_ind[2];
+    char *FAT;
+    // 保留扇区结束后即第一个FAT表,
+    // 第二个FAT表还需加上第一个FAT表的扇区数
+    FAT_ind[0] = bootEntry.BPBReservedSectorCount;
+    FAT_ind[1] = bootEntry.BPBReservedSectorCount + bootEntry.PBPSectorPerFAT;
+  }
+  return 0;
+}
 
 /*
 #define HEADSIZE 42
