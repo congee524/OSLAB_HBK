525dd07ae4895d176399de2c60b008c0c37d3a57
tracer-nju <tracer@nju.edu.cn>
2019-05-28 22:24:06 +0800

    Static hostname: Inspiron          Icon name: computer-laptop            Chassis: laptop         Machine ID: 537ec2aa89e64954b7d607f1de70fb4e            Boot ID: 30c57d3492a6408494b45f2b66813e7e   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.15.0-51-generic       Architecture: x86-64  22:24:06 up  1:31,  3 users,  load average: 1.92, 1.89, 1.62

--- frecov/frecov.c
+++ frecov/frecov.c
@@ -106,8 +106,8 @@ typedef struct {
 
 char *trname(LFNEntry *LFN, char *nbuffer) {
   char tmp_na;
-  for (int i = 4; i >= 0; i--) {
-    tmp_na = (char)LFN->Name1[i];
+  for (int i = 1; i >= 0; i--) {
+    tmp_na = (char)LFN->Name3[i];
     // if (tmp_na >= 0x30 && tmp_na <= 0x7e) *(--nbuffer) = tmp_na;
     // if (tmp_na) *(--nbuffer) = tmp_na;
     *(nbuffer++) = tmp_na;
@@ -118,8 +118,8 @@ char *trname(LFNEntry *LFN, char *nbuffer) {
     // if (tmp_na) *(--nbuffer) = tmp_na;
     *(nbuffer++) = tmp_na;
   }
-  for (int i = 1; i >= 0; i--) {
-    tmp_na = (char)LFN->Name3[i];
+  for (int i = 4; i >= 0; i--) {
+    tmp_na = (char)LFN->Name1[i];
     // if (tmp_na >= 0x30 && tmp_na <= 0x7e) *(--nbuffer) = tmp_na;
     // if (tmp_na) *(--nbuffer) = tmp_na;
     *(nbuffer++) = tmp_na;
