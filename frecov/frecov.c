aefc570f525c71572a1f855053dda7bbbba8233e
tracer-nju <tracer@nju.edu.cn>
2019-05-28 22:48:30 +0800

    Static hostname: Inspiron          Icon name: computer-laptop            Chassis: laptop         Machine ID: 537ec2aa89e64954b7d607f1de70fb4e            Boot ID: 30c57d3492a6408494b45f2b66813e7e   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.15.0-51-generic       Architecture: x86-64  22:48:30 up  1:55,  3 users,  load average: 3.67, 2.48, 2.06

--- frecov/frecov.c
+++ frecov/frecov.c
@@ -185,34 +185,35 @@ int main(int argc, char *argv[]) {
         memset(tmp_name, '\0', sizeof(tmp_name));
 
         LFNEntry *LFN = (LFNEntry *)(addr + pos - 32);
-        if (LFN->Attr == 0x0f) {
-          char name_buffer[32];
-          memset(name_buffer, '\0', sizeof(name_buffer));
-          char *nbuffer = &name_buffer[32];
+        // if (LFN->Attr == 0x0f) {
+        char name_buffer[32];
+        memset(name_buffer, '\0', sizeof(name_buffer));
+        char *nbuffer = &name_buffer[32];
 
-          while (LASTDIR(LFN->SequeNumber) != 1) {
-            LFN--;
-          }
-          while (LFN->Attr == 0x0f) {
-            nbuffer = trname(LFN, nbuffer);
-            LFN++;
-          }
-          memcpy(tmp_name, nbuffer, 32);
-          if (tmp_name[0] < 0x30 || tmp_name[1] > 0x7a) continue;
-          printf("%s\n", tmp_name);
-        } else {
-          memcpy(tmp_name, dirE->Name, 8);
-          memcpy(tmp_name + strlen(tmp_name), ".", 1);
-          memcpy(tmp_name + strlen(tmp_name), dirE->ExtendName, 3);
-          printf("%s\n", tmp_name);
+        while (LASTDIR(LFN->SequeNumber) != 1) {
+          LFN--;
         }
-
-        int fcluster =
-            dirE->FileStartClusterLow + (dirE->FileStartClusterHigh << 16);
-        int Bmp_SecNum = data_SecNum + (fcluster - 2) * spc;
-        FILE *outfp = fopen(tmp_name, "a");
-        fwrite(addr + (Bmp_SecNum * bps), dirE->FileSize, 1, outfp);
-        fclose(outfp);
+        while (LFN->Attr == 0x0f) {
+          nbuffer = trname(LFN, nbuffer);
+          LFN++;
+        }
+        memcpy(tmp_name, nbuffer, 32);
+        if (tmp_name[0] < 0x30 || tmp_name[1] > 0x7a) continue;
+        printf("%s\n", tmp_name);
+        /*
+      } else {
+       memcpy(tmp_name, dirE->Name, 8);
+        memcpy(tmp_name + strlen(tmp_name), ".", 1);
+        memcpy(tmp_name + strlen(tmp_name), dirE->ExtendName, 3);
+        printf("%s\n", tmp_name);
+      }*/
+        /*
+                int fcluster =
+                    dirE->FileStartClusterLow + (dirE->FileStartClusterHigh <<
+           16); int Bmp_SecNum = data_SecNum + (fcluster - 2) * spc; FILE *outfp
+           = fopen(tmp_name, "a"); fwrite(addr + (Bmp_SecNum * bps),
+           dirE->FileSize, 1, outfp); fclose(outfp);
+                */
       }
     }
   }
