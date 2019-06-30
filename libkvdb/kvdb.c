ac253513dc3a157a24fe1a33650a577746d87fbc
tracer-nju <tracer@nju.edu.cn>
2019-06-30 11:04:12 +0800

    Static hostname: Inspiron          Icon name: computer-laptop            Chassis: laptop         Machine ID: 537ec2aa89e64954b7d607f1de70fb4e            Boot ID: 7122cf0119dc401cb47f36cec8148616   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.15.0-52-generic       Architecture: x86-64  11:04:13 up 17:46,  1 user,  load average: 1.67, 1.26, 1.27

--- libkvdb/kvdb.c
+++ libkvdb/kvdb.c
@@ -1,13 +1,10 @@
 #include "kvdb.h"
-char *key_buf;
-char *val_buf;
 
 // 文件里是key 和 value一行接着一行，读到最后一个
 int kvdb_open(kvdb_t *db, const char *filename) {
   strcpy(db->name, filename);
   db->fp = fopen(filename, "a+");
-  key_buf = malloc(2048);
-  val_buf = malloc(2048);
+
   return 0;
 }
 
@@ -36,7 +33,8 @@ char *kvdb_get(kvdb_t *db, const char *key) {
     printf("the file was locked\n");
   }
   fseek(db->fp, 0, SEEK_SET);
-
+  char *key_buf = malloc(2048);
+  char *val_buf = malloc(2048);
   while (1) {
     if (!fgets(key_buf, 2048, db->fp)) break;
     if (!fgets(val_buf, 2048, db->fp)) break;
@@ -44,5 +42,6 @@ char *kvdb_get(kvdb_t *db, const char *key) {
   if (flock(db->fp->_fileno, LOCK_UN) == 0) {
     printf("the file was unlocked\n");
   }
+  free(key_buf);
   return val_buf;
 }
