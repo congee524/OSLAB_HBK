d8600d09d36f02d4ec3ac6c7e6de92f69625f66f
tracer-nju <tracer@nju.edu.cn>
2019-06-30 14:34:55 +0800

    Static hostname: Inspiron          Icon name: computer-laptop            Chassis: laptop         Machine ID: 537ec2aa89e64954b7d607f1de70fb4e            Boot ID: 7122cf0119dc401cb47f36cec8148616   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.15.0-52-generic       Architecture: x86-64  14:34:55 up 21:16,  1 user,  load average: 1.62, 1.98, 2.07

--- libkvdb/kvdb.c
+++ libkvdb/kvdb.c
@@ -4,21 +4,40 @@ int errlog(char *buf) { return printf("\33[1;33m %s \33[0m\n", buf); }
 
 // 文件里是key 和 value一行接着一行，读到最后一个
 int kvdb_open(kvdb_t *db, const char *filename) {
+  if (pthread_mutex_lock(&db->mutex)) {
+    errlog("close mutex lock error");
+    return -1;
+  }
   strcpy(db->name, filename);
   if ((db->fp = fopen(filename, "a+")) == NULL) {
     errlog("file open fail!");
     return -1;
   };
-
+  if (pthread_mutex_unlock(&db->mutex)) {
+    errlog("close mutex unlock error");
+    return -1;
+  }
   return 0;
 }
 
 int kvdb_close(kvdb_t *db) {
+  if (pthread_mutex_lock(&db->mutex)) {
+    errlog("close mutex lock error");
+    return -1;
+  }
   fclose(db->fp);
+  if (pthread_mutex_unlock(&db->mutex)) {
+    errlog("close mutex unlock error");
+    return -1;
+  }
   return 0;
 }
 
 int kvdb_put(kvdb_t *db, const char *key, const char *value) {
+  if (pthread_mutex_lock(&db->mutex)) {
+    errlog("close mutex lock error");
+    return -1;
+  }
   if (flock(db->fp->_fileno, LOCK_EX) == 0) {
     printf("the file was locked\n");
   }
@@ -28,10 +47,18 @@ int kvdb_put(kvdb_t *db, const char *key, const char *value) {
   if (flock(db->fp->_fileno, LOCK_UN) == 0) {
     printf("the file was unlocked\n");
   }
+  if (pthread_mutex_unlock(&db->mutex)) {
+    errlog("close mutex unlock error");
+    return -1;
+  }
   return 0;
 }
 
 char *kvdb_get(kvdb_t *db, const char *key) {
+  if (pthread_mutex_lock(&db->mutex)) {
+    errlog("close mutex lock error");
+    return -1;
+  }
   if (flock(db->fp->_fileno, LOCK_EX) == 0) {
     printf("the file was locked\n");
   }
@@ -47,5 +74,9 @@ char *kvdb_get(kvdb_t *db, const char *key) {
   }
   free(key_buf);
   val_buf[strlen(val_buf) - 1] = '\0';
+  if (pthread_mutex_unlock(&db->mutex)) {
+    errlog("close mutex unlock error");
+    return -1;
+  }
   return val_buf;
 }
