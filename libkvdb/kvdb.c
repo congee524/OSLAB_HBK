7bb658258e73413f647758bb9121041c32fba96c
171240513 <171240513@smail.nju.edu.cn>
2019-06-30 10:49:27 +0800

    implement basic function

--- libkvdb/kvdb.c
+++ libkvdb/kvdb.c
@@ -1,2 +1,44 @@
 #include "kvdb.h"
 
+// 文件里是key 和 value一行接着一行，读到最后一个
+int kvdb_open(kvdb_t *db, const char *filename) {
+  strcpy(db->name, filename);
+  db->fp = fopen(filename, "a+");
+  return 0;
+}
+
+int kvdb_close(kvdb_t *db) {
+  fclose(db->fp);
+  return 0;
+}
+
+int kvdb_put(kvdb_t *db, const char *key, const char *value) {
+  if (flock(db->fp->_fileno, LOCK_EX) == 0) {
+    printf("the file was locked\n");
+  }
+  fseek(db->fp, 0, SEEK_END);
+  fprintf(db->fp, key);
+  fprintf(db->fp, "\n");
+  fprintf(db->fp, value);
+  fprintf(db->fp, "\n");
+  if (flock(db->fp->_fileno, LOCK_UN) == 0) {
+    printf("the file was unlocked\n");
+  }
+}
+
+char *kvdb_get(kvdb_t *db, const char *key) {
+  if (flock(db->fp->_fileno, LOCK_EX) == 0) {
+    printf("the file was locked\n");
+  }
+  fseek(db->fp, 0, SEEK_SET);
+  char key_buf[2048];
+  char val_buf[2048];
+  while (1) {
+    if (!fgets(key_buf, 2048, db->fp)) break;
+    if (!fgets(val_buf, 2048, db->fp)) break;
+  }
+  if (flock(db->fp->_fileno, LOCK_UN) == 0) {
+    printf("the file was unlocked\n");
+  }
+  return val_buf;
+}
--- libkvdb/kvdb.h
+++ libkvdb/kvdb.h
@@ -1,8 +1,19 @@
 #ifndef __KVDB_H__
 #define __KVDB_H__
-
+#include <errno.h>
+#include <fcntl.h>
+#include <malloc.h>
+#include <pthread.h>
+#include <stdio.h>
+#include <sys/file.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
 
 struct kvdb {
+  char name[128];
+  FILE *fp;
+  pthread_mutex_t mutex;
 };
 typedef struct kvdb kvdb_t;
 
