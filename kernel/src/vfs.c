c91b74c3bf56655ca5581dc613f8be454ad68d4d
tracer-nju <tracer@nju.edu.cn>
2019-07-13 21:55:13 +0800

    Static hostname: Inspiron          Icon name: computer-laptop            Chassis: laptop         Machine ID: 537ec2aa89e64954b7d607f1de70fb4e            Boot ID: a9fb6b488fa84515bc31e0714a0b6d8d   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.15.0-54-generic       Architecture: x86-64  21:55:13 up  8:12,  1 user,  load average: 1.65, 1.21, 1.13

--- kernel/src/vfs.c
+++ kernel/src/vfs.c
@@ -47,7 +47,7 @@ int vfs_mount(const char *path, filesystem_t *fs) {
   resolvedpath = realpath(path, resolvedpath);
   if (!resolvedpath) return -1;
 
-  mptable[mptable_cnt].path = path;
+  mptable[mptable_cnt].mount_point = path;
   mptable[mptable_cnt++].fs = fs;
   return 0;
 }
