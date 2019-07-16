b823bf89489167d17bdbaf875c026e517fd50718
171240513 <171240513@smail.nju.edu.cn>
2019-07-16 23:22:27 +0800

    implement b_inode block size 0x80 the maximum of file size is 4096 byte we have at most 0x8000 files and at least 0x400 files

---
+++ kernel/include/blkfs.h
@@ -0,0 +1,20 @@
+#ifndef __BLKFS_H__
+#define __BLKFS_H__
+#include <common.h>
+// 需要做一个该文件系统的inode，就像ext2那样，inode额外做吧！
+typedef struct blkfs_inode b_inode
+// 直接指针32个，每块128(0x80)字节，文件最大4096字节
+// 最多32768(0x8000)个文件，最少1024(0x400)个文件
+#define DIREPOINTNUM 32
+#define BLOCK_SIZE 0x80
+#define BLOCK_NUM 0x8000
+
+    struct blkfs_inode {
+  size_t fsize;                      //文件大小
+  uint32_t mode;                     // 文件的读写执行权限
+  int link_num;                      // 文件的链接数
+  int type;                          // 文件类型
+  uint16_t ptr_point[DIREPOINTNUM];  // 文件内容指针
+}
+
+#endif
