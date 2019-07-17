d8f98be3f07bbe9dda7be816ceddf53e833a8e04
tracer-nju <tracer@nju.edu.cn>
2019-07-17 14:44:44 +0800

    Static hostname: Inspiron          Icon name: computer-laptop            Chassis: laptop         Machine ID: 537ec2aa89e64954b7d607f1de70fb4e            Boot ID: d29a8a9d386f4358b1cc5ff6dd06b151   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.15.0-55-generic       Architecture: x86-64  14:44:44 up 16:05,  3 users,  load average: 2.20, 1.83, 1.85

--- kernel/src/fs/blkfs.c
+++ kernel/src/fs/blkfs.c
@@ -116,7 +116,8 @@ ssize_t blkfs_iwrite(file_t *file, const char *buf, size_t size) {
   }
   int block_fid = file->offset / BLOCK_SIZE;
   size_t pre_block_off = file->offset - block_fid * BLOCK_SIZE;
-  char *tmp_buf = buf;
+  char tmp_buf[size];
+  strcpy(tmp_buf, buf);
   size_t tmp_size = size;
   while (tmp_size + pre_block_off >= BLOCK_SIZE) {
     size_t nwrite = dev->ops->write(
