bc7d1f3ecacb645aaf5968f0a298f34eec91ccc1
tracer-nju <tracer@nju.edu.cn>
2019-07-17 22:53:46 +0800

    Static hostname: Inspiron          Icon name: computer-laptop            Chassis: laptop         Machine ID: 537ec2aa89e64954b7d607f1de70fb4e            Boot ID: d29a8a9d386f4358b1cc5ff6dd06b151   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.15.0-55-generic       Architecture: x86-64  22:53:47 up 1 day, 14 min,  4 users,  load average: 4.06, 3.33, 3.36

--- kernel/src/cmd.c
+++ kernel/src/cmd.c
@@ -13,8 +13,10 @@ extern inode_t *itable[];
 
 int cmd_parse(char *input, char *output) {
   memset(output, '\0', sizeof(output));
-  char *tmp_cmd = pmm->alloc(1024);
+  char *tmp_cmd = pmm->alloc(128);
+  printf("input: %s\n", input);
   strcpy(tmp_cmd, input);
+  printf("tmp_cmd: %s\n", tmp_cmd);
   char *pch = strtok(tmp_cmd, " ");
   int cmd_type = 0;
   char *temp = NULL;
