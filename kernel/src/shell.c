b8c9a6ec81fd84a38d9c3197ad14747a81b9235a
tracer-nju <tracer@nju.edu.cn>
2019-07-17 15:41:30 +0800

    Static hostname: Inspiron          Icon name: computer-laptop            Chassis: laptop         Machine ID: 537ec2aa89e64954b7d607f1de70fb4e            Boot ID: d29a8a9d386f4358b1cc5ff6dd06b151   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.15.0-55-generic       Architecture: x86-64  15:41:30 up 17:02,  3 users,  load average: 2.21, 1.80, 2.04

--- kernel/src/shell.c
+++ kernel/src/shell.c
@@ -23,7 +23,7 @@ void shell_thread(void *tty_id) {
     int nread = vfs->read(stdin, line, strlen(text));
     // printf("sh3\n");
     line[nread - 1] = '\0';
-    realpath(line, tran);
+    int tmp_id = find_parent_di(line, tran);
     sprintf(text, "Echo: %s\n", tran);
     vfs->write(stdout, text, strlen(text));
     // printf("sh4\n");
