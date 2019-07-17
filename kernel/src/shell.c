0fbb1c5820e3ed21ee7fb02a0513a6cca099df77
tracer-nju <tracer@nju.edu.cn>
2019-07-17 15:15:52 +0800

    Static hostname: Inspiron          Icon name: computer-laptop            Chassis: laptop         Machine ID: 537ec2aa89e64954b7d607f1de70fb4e            Boot ID: d29a8a9d386f4358b1cc5ff6dd06b151   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.15.0-55-generic       Architecture: x86-64  15:15:52 up 16:36,  3 users,  load average: 1.33, 1.51, 1.60

--- kernel/src/shell.c
+++ kernel/src/shell.c
@@ -14,7 +14,7 @@ void shell_thread(void *tty_id) {
   int stdin = vfs->open(buf, O_RDONLY);
   int stdout = vfs->open(buf, O_WRONLY);
   while (1) {
-    char line[128], text[128];
+    char line[128], text[128], tran[128];
     // printf("sizeof text before: %d", strlen(text));
     sprintf(text, "(%s) [%s] $ ", buf, cur_pwd);
 
@@ -23,7 +23,8 @@ void shell_thread(void *tty_id) {
     int nread = vfs->read(stdin, line, strlen(text));
     // printf("sh3\n");
     line[nread - 1] = '\0';
-    sprintf(text, "Echo: %s.\n", line);
+    realpath(line, tran);
+    sprintf(text, "Echo: %s.\n", tran);
     vfs->write(stdout, text, strlen(text));
     // printf("sh4\n");
   }
